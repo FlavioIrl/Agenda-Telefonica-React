3cf90f878be6977d94aa411abb17487fc0515f53
Flávio Irala <flavioiralap@gmail.com>
2025-05-01 21:37:43 -0400

    Contato adicionado

--- src/components/Contato/index.tsx
+++ src/components/Contato/index.tsx
@@ -1,16 +1,27 @@
-import { ContatoCard } from './styles'
+import { Card, Titulo, Botoes, Info, Descricao, BarraAcoes, BotaoEditar, BotaoRemover, CardContainer } from './styles'
 
 const Contato = () => {
-    return (
-        <div>
-            <ContatoCard>
-                <li><p>Flavio Irala Pereira</p></li>
-                <li><p>flavio@gmail.com</p></li>
-                <li><p>37 9 9999-9999</p></li>
-                <li><p>Eletronica Parana</p></li>
-            </ContatoCard>
-        </div>
-    )
+  return (
+    <CardContainer>
+        <Card>
+            <BarraAcoes>
+                <Titulo>Nome: Flavio Irala Pereira</Titulo>
+                <Botoes>
+                    <BotaoEditar><img src="../../public/icons8-edit.png" /></BotaoEditar>
+                    <BotaoRemover><img src="../../public/icons8-lixeira.png" /></BotaoRemover>
+                </Botoes>
+            </BarraAcoes>
+            <Info>
+                <p>Email: flavio@gmail.com</p>
+                <p>telefone: 37 9 9999-9999</p>
+                <p>Empresa: Eletrônica Paraná</p>
+            </Info>
+            <Descricao>
+                Contato importante da área técnica.
+            </Descricao>
+        </Card>
+    </CardContainer>
+  )
 }
 
 export default Contato
--- src/components/Contato/styles.ts
+++ src/components/Contato/styles.ts
@@ -1,7 +1,93 @@
-import styled from "styled-components"
+import styled from 'styled-components'
+import { BotaoBase } from '../../styles'
 
-export const ContatoCard = styled.div`
-    width: 900px;
-    background-color: #cacaca;
+export const CardContainer = styled.div`
+max-width: 90%;
+font-family: Roboto, sans-serif;
+`
+
+
+export const Card = styled.div`
+background-color: #cacaca;
+padding: 8px;
+margin-left: 30px; 
+border-radius: 12px;
+`
+
+export const Titulo = styled.h2`
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 8px;
+    color: #333;
+`
+
+
+export const Info = styled.div`
+display: flex;
+
+p {
+margin-right: 80px;
+font-size: 14px;
+color: #555;
+}
+
+`
+
+
+export const Descricao = styled.p`
+margin-top: 12px;
+font-size: 13px;
+color: #888;
+font-style: italic;
+
+`
+
+
+export const Botoes = styled.div`
     display: flex;
+    gap: 8px;
+`
+
+
+export const BarraAcoes = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+`
+
+export const BotaoEditar = styled(BotaoBase)`
+background-color: #527AD0;
+color: white;
+width: 40px;
+height: 40px;
+margin-right: 10px;
+
+img {    
+  width: 100%;
+}
+&:hover {
+  background-color: #2d2d2d;
+}
+
+`
+
+
+export const BotaoRemover = styled(BotaoBase)`
+background-color: #DF6161;
+color: white;
+
+width: 40px;
+height: 40px;
+
+img {    
+  width: 100%;
+}
+
+&:hover {
+  background-color: #c9302c;
+}
+
+
+
 `
